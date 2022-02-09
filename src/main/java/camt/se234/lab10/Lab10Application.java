708ab82e14d77bc54f30f2383793122adbbcb4b5
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2022-02-10 00:40:36 +0700

    add cors Filter

--- src/main/java/camt/se234/lab10/Lab10Application.java
+++ src/main/java/camt/se234/lab10/Lab10Application.java
@@ -9,17 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class Lab10Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab10Application.class, args);
-	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000", "http://52.74.162.217:8086/");
-			}
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab10Application.class, args);
+    }
+
+
 }
