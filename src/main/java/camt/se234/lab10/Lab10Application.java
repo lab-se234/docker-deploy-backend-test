eba679cc6d7734ab56e48d6792d578e458d12cde
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2022-02-10 00:05:42 +0700

    add cors origin

--- src/main/java/camt/se234/lab10/Lab10Application.java
+++ src/main/java/camt/se234/lab10/Lab10Application.java
@@ -2,11 +2,24 @@ package camt.se234.lab10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lab10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab10Application.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "http://52.74.162.217:8086");
+			}
+		};
 	}
 }
